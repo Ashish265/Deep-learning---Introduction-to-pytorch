3d991d8dbd1a665fbb2f319e7c6b6668c79ea614
Ashish Gupta <ashishgupta2692@gmail.com>
2026-05-13 23:07:23 +0530

    Add files via upload
    gradient practise

--- gradient.py
+++ gradient.py
@@ -12,3 +12,12 @@ x = torch.tensor(2.0, requires_grad=True)
 y =x**2 + 2*x + 1
 y.backward()
 print(x.grad)
+
+
+#complex function gradient
+x = torch.tensor(1.0, requires_grad=True)
+y = x**3 + 2*x**2 + 3*x + 4
+y.backward()
+print(x.grad)
+
+
